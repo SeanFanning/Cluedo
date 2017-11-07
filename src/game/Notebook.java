2fc02e5e6430ec5606a3c75e2bab71f52e321b41
Sean Fanning <seanfanning00@gmail.com>
2017-11-07 13:40:39 +0000

    Added comments to Notebook class Added Sequence Diagram

--- src/game/Notebook.java
+++ src/game/Notebook.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Notebook {
 
+    // An array list of strings will store the notes
     private List<String> notes = new ArrayList<String>();
     private String name;
 
@@ -12,10 +13,12 @@ public class Notebook {
         add_note(name + "\'s Notebook: ");
     }
 
+    // Function to add a string to the notebook
     public void add_note(String n){
         notes.add(n);
     }
 
+    // Function which prints each line in the notebook
     public void read_notebook(){
         for(int i=0; i<notes.size(); i++){
             System.out.println(notes.get(i));
