55217fd502ecbb1e144f4bc39e854e3d3fa53bd8
Sean Fanning <seanfanning00@gmail.com>
2017-11-20 14:54:36 +0000

    Slots

--- src/board/Map.java
+++ src/board/Map.java
@@ -1,14 +1,18 @@
 package board;
 
-public class Map {
-    //private int[][] map = new int[8][8];
+import game.Card;
+import game.Rooms;
 
+public class Map {
     private char[][] map = new char[][]{
-        { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
-        { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-        { 'X', 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', 'X' },
-        { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-        { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
+            { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', 'X', 'C', 'C', 'C', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', 'C', 'C', 'C', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', 'X', 'C', 'C', 'C', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', 'X', 'C', 'C', 'C', 'X' },
+            { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
     };
 
     public boolean canMove(int x, int y){
@@ -22,6 +26,35 @@ public class Map {
 
     public char[][] getMap(){
         return map;
+    }
+
+    public Card getSlot(int x, int y){
+        char c = map[x][y];
+        Card card;
+
+        switch (c){
+            case 'K':   card = Rooms.KITCHEN;
+                break;
+            case 'B':   card = Rooms.BALLROOM;
+                break;
+            case 'C':   card = Rooms.CONSERVATORY;
+                break;
+            case 'L':   card = Rooms.LIBRARY;
+                break;
+            case 'l':   card = Rooms.LOUNGE;
+                break;
+            case 'b':   card = Rooms.BILLIARD_ROOM;
+                break;
+            case 'D':   card = Rooms.DINING_ROOM;
+                break;
+            case 'S':   card = Rooms.STUDY;
+                break;
+            case 'H':   card = Rooms.HALL;
+                break;
+            default:    return null;
+        }
+
+        return card;
     }
 
     //TODO: DFS Search to get distance to destination, while moving around walls etc.
