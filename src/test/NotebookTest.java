1bfab8450d03d1a01dc9abfab09638d28b1956ae
Sean Fanning <seanfanning00@gmail.com>
2017-12-22 17:16:16 +0000

    Added tests for the Notebook

--- src/test/NotebookTest.java
+++ src/test/NotebookTest.java
@@ -13,15 +13,34 @@ public class NotebookTest {
     @Before
     public void setUp() throws Exception {
         notebook = new Notebook("PROFESSOR_PLUM");
+        notebook.add_note("Test note in notebook", "Test",false);
     }
 
     @Test
     public void testNotebook() {
-        // assert statements
-        assertEquals(notebook.getNotes().get(0).getNote(), "PROFESSOR_PLUM\'s Notebook: ");
-        assertNotEquals(notebook.getNotes().get(0).getNote(), "PROFESSOR_PLUM\'s Notebook:");
-        notebook.add_note("Test note in notebook", "Test",false);
-        assertEquals(notebook.getNotes().get(1).getNote(), "Test note in notebook");
+        // Test basic notes
+        assertEquals("PROFESSOR_PLUM\'s Notebook: ", notebook.getNotes().get(0).getNote());
+        assertNotEquals("REVEREND\'s Notebook: ", notebook.getNotes().get(0).getNote());
+
+        assertEquals("Test note in notebook", notebook.getNotes().get(1).getNote());
+    }
+
+    @Test
+    public void testNoteTypes(){
+        // Test note types
+        assertEquals("Test", notebook.getNotes().get(1).getType());
+    }
+
+    @Test
+    public void testFilteredNotes(){
+        // Test filtering notes
+        notebook.add_note("Another test note in notebook", "Test",false);
+        notebook.add_note("This note has a different type", "Other",false);
+        notebook.add_note("One more test note in notebook", "Test",false);
+        notebook.add_note("Another Other note", "Other",false);
+
+        assertEquals("Another Other note", notebook.filterNotes("Other").get(1).getNote());
+        assertEquals("Other", notebook.filterNotes("Other").get(0).getType());
     }
 
 }
